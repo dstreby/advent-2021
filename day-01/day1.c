61fee7db0b864099459203f3338f79f805a91959
Daniel Streby <dstreby@2u.com>
2021-12-03 10:06:46 -0500

    Correct day 1 solution to prevent array overshoot

--- day-01/day1.c
+++ day-01/day1.c
@@ -30,14 +30,14 @@ int main ( int argc, char *argv[] )
 {
   FILE *fp = fopen("input", "r");
 
-  int num = 0;
-  int nums = 0;
   if ( fp == NULL ) {
     printf ( "Unable to read from file\n" );
     return EXIT_FAILURE;
   }
 
   // get number of ints in file
+  int num = 0;
+  int nums = 0;
   while ( fscanf(fp, "%d", &num) != EOF ) {
     nums++;
   }
@@ -61,12 +61,19 @@ int main ( int argc, char *argv[] )
   int arrLen = sizeof(depth) / sizeof(depth[0]);
   int increases = 0;
   int windowIncs = 0;
-  for(int i = 1; i < arrLen; i++) {
+  // stop the loop one element from the end, since the last integer
+  // has no subsequent integer to compare to
+  for(int i = 0; i < arrLen - 1; i++) {
     if ( depth[i+1] > depth[i]  ) {
       increases++;
     }
-    if ( (depth[i+1] + depth[i+2] + depth[i+3]) > (depth[i] + depth[i+1] + depth[i+2]) ) {
-      windowIncs++;
+    // only evaluate the sliding window until the array iterator
+    // is 4 elements from the end of the array so we don't overshoot
+    // i.e. at i = arrLen - 4; depth[i+3] is the last element in the array
+    if ( i < ( arrLen - 3 ) ) {
+      if ( (depth[i+1] + depth[i+2] + depth[i+3]) > (depth[i] + depth[i+1] + depth[i+2]) ) {
+        windowIncs++;
+      }
     }
   }
 
