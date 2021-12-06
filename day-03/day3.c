c81428aafdab52b8953811ceef1153801d5a4d87
Daniel Streby <dstreby@2u.com>
2021-12-06 10:36:17 -0500

    Add day3 part 2 solution

--- day-03/day3.c
+++ day-03/day3.c
@@ -1,7 +1,7 @@
 /*
  * =====================================================================================
  *
- *       Filename:  day3.c
+ *       Filename:  day3_pt2.c
  *
  *    Description:  
  *
@@ -22,24 +22,152 @@
 #include <math.h>
 
 #define BUFF_SIZE 16
+#define NUM_BITS 12
 
 /* 
  * ===  FUNCTION  ======================================================================
- *         Name:  bin_to_dec
+ *         Name:  binarray_to_dec
  *  Description:  
  * =====================================================================================
  */
-int bin_to_dec ( long bin )
+int binarray_to_dec ( int *binArray, int arrayLen )
 {
-  int dec = 0;
+  int decimal = 0;
+  for ( int i = 0; i < arrayLen; i++ ) {
+    int power = arrayLen - i - 1;
+    decimal +=  binArray[i] * pow(2, power);
+  }
+  return decimal;
+}
 
-  for ( int i = 0; bin; i++, bin /= 10) {
-    if ( bin % 10 ) {
-      dec += pow(2, i);
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  reduce_by_minor
+ *  Description:  
+ * =====================================================================================
+ */
+void reduce_by_minor ( int *binArray, int rows, int cols, int reducedRows, int minorBit, int index, int *reduced)
+{
+  int reducedIndex = 0;
+  int reducedArray[reducedRows][cols];
+  memset(reducedArray, 0, sizeof(reducedArray[0][0]) * reducedRows * cols);
+  for ( int i = 0; i < rows; i++ ) {
+    if ( *(binArray + i*cols + index) == minorBit ) {
+      for ( int j = 0; j < cols; j++ ) {
+        reducedArray[reducedIndex][j] = *(binArray + i*cols + j);
+      }
+      reducedIndex++;
     }
   }
 
-  return dec;
+  index++;
+
+  // Count the number of 1's and 0's in the indexed bit position of the array
+  int firstBitCount[2] = { 0, 0 };
+  for ( int x = 0; x < reducedRows; x++ ) {
+    if ( reducedArray[x][index] == 0 ) {
+      firstBitCount[0]++;
+    } else if ( reducedArray[x][index] == 1 ) {
+      firstBitCount[1]++;
+    }
+  }
+
+  int nextMinorBit = 0;
+  if ( ( firstBitCount[0] + firstBitCount[1] ) == 1 ) {
+    int final[cols];
+    memset(final, 0, cols*sizeof(final[0]));
+    for ( int k = 0; k < reducedRows; k++ ) {
+      for ( int l = 0; l < cols; l++ ) {
+        final[l] = reducedArray[k][l];
+      }
+    }
+    *reduced = binarray_to_dec(final, cols);
+    return;
+  } else if ( firstBitCount[0] == firstBitCount[1] ) {
+    int final[cols];
+    memset(final, 0, cols*sizeof(final[0]));
+    for ( int k = 0; k < reducedRows; k++ ) {
+      if ( reducedArray[k][index] == 0 ) {
+        for ( int l = 0; l < cols; l++ ) {
+          final[l] = reducedArray[k][l];
+        }
+        *reduced = binarray_to_dec(final, cols);
+        return;
+      }
+    }
+  } else if ( firstBitCount[0] < firstBitCount[1] ) {
+    nextMinorBit = 0;
+  } else if ( firstBitCount[1] < firstBitCount[0] ) {
+    nextMinorBit = 1;
+  }
+
+  reduce_by_minor(reducedArray[0], reducedRows, cols, firstBitCount[nextMinorBit], nextMinorBit, index, reduced);
+
+}
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  reduce_by_dominant
+ *  Description:  
+ * =====================================================================================
+ */
+void reduce_by_dominant ( int *binArray, int rows, int cols, int reducedRows, int domBit, int index, int *reduced)
+{
+  int reducedIndex = 0;
+  int reducedArray[reducedRows][cols];
+  memset(reducedArray, 0, sizeof(reducedArray[0][0]) * reducedRows * cols);
+  for ( int i = 0; i < rows; i++ ) {
+    if ( *(binArray + i*cols + index) == domBit ) {
+      for ( int j = 0; j < cols; j++ ) {
+        reducedArray[reducedIndex][j] = *(binArray + i*cols + j);
+      }
+      reducedIndex++;
+    }
+  }
+
+  index++;
+
+  // Count the number of 1's and 0's in the indexed bit position of the array
+  int firstBitCount[2] = { 0, 0 };
+  for ( int x = 0; x < reducedRows; x++ ) {
+    if ( reducedArray[x][index] == 0 ) {
+      firstBitCount[0]++;
+    } else if ( reducedArray[x][index] == 1 ) {
+      firstBitCount[1]++;
+    }
+  }
+
+  int dominantBit = 0;
+  if ( ( firstBitCount[0] + firstBitCount[1] ) == 1 ) {
+    int final[cols];
+    memset(final, 0, cols*sizeof(final[0]));
+    for ( int k = 0; k < reducedRows; k++ ) {
+      for ( int l = 0; l < cols; l++ ) {
+        final[l] = reducedArray[k][l];
+      }
+    }
+    *reduced = binarray_to_dec(final, cols);
+    return;
+  } else if ( firstBitCount[0] == firstBitCount[1] ) {
+    int final[cols];
+    memset(final, 0, cols*sizeof(final[0]));
+    for ( int k = 0; k < reducedRows; k++ ) {
+      if ( reducedArray[k][index] == 1 ) {
+        for ( int l = 0; l < cols; l++ ) {
+          final[l] = reducedArray[k][l];
+        }
+        *reduced = binarray_to_dec(final, cols);
+        return;
+      }
+    }
+  } else if ( firstBitCount[0] > firstBitCount[1] ) {
+    dominantBit = 0;
+  } else if ( firstBitCount[1] > firstBitCount[0] ) {
+    dominantBit = 1;
+  }
+
+  reduce_by_dominant(reducedArray[0], reducedRows, cols, firstBitCount[dominantBit], dominantBit, index, reduced);
+
 }
 
 /* 
@@ -57,53 +185,93 @@ int main ( int argc, char *argv[] )
     return EXIT_FAILURE;
   }
 
-  // Array to determine if there are more 1's or 0's for each "bit" position in the
-  // input data strings. Once processing is complete, the "bit" value will be negative if
-  // there were more 0's in that field, or positive if there were more 1's in that field
-  int digits[12] = {0};
-  int line = 0;
+  int bitCount[NUM_BITS][2] = {0};
+  int bit = 0;
+  int rows = 0;
   char buffer[BUFF_SIZE];
   // Iterate through the file, loading one string at a time into buffer
   while ( fscanf(fp, "%s", buffer) != EOF ) {
-    fprintf ( stderr, "Line: %04d, Buffer: %s ", line, buffer );
+    //#DEBUG fprintf ( stderr, "Line: %04d, Buffer: %s ", lines, buffer );
     char *buffChar = buffer;
-    int bit = 0;
+    bit = 0;
     // Iterate through buffer, one character at a time (until NULL terminator)
-    // and increment / decrement the digits array for each "bit" position.
+    // and increment the bitCount array for each "bit" position / value.
     while ( buffChar != NULL && *buffChar != '\0' ) {
-      fprintf ( stderr, "%c ", *buffChar );
+      //#DEBUG fprintf ( stderr, "%c ", *buffChar );
       if ( *buffChar == '0') {
-        digits[bit]--;
+        bitCount[bit][0]++;
       } else if ( *buffChar == '1') {
-        digits[bit]++;
+        bitCount[bit][1]++;
       }
       ++buffChar;
       ++bit;
     }
-    fprintf ( stderr, "\n" );
+    //#DEBUG fprintf ( stderr, "\n" );
+    rows++;
+  }
+
+  // set the file pointer back to the start of the file
+  fseek(fp, 0, SEEK_SET);
+
+  // For part2, create a 2D array to hold each "bit" as an int
+  // we can reuse buffer and bit from above
+  int dataset[rows][NUM_BITS];
+  memset(dataset, 0, sizeof(dataset[0][0]) * rows * NUM_BITS);
+  int line = 0;
+
+  while ( fscanf(fp, "%s", buffer) != EOF ) {
+    char *buffChar = buffer;
+    bit = 0;
+    // Iterate through buffer, one character at a time (until NULL terminator)
+    while ( buffChar != NULL && *buffChar != '\0' ) {
+      dataset[line][bit] = *buffChar - '0';
+      ++buffChar;
+      ++bit;
+    }
     line++;
   }
+
   fclose(fp);
 
-  // Construct the binary string for gamma and epsilon values bitwise
-  char gamma[] = "000000000000";
-  char epsilon[] = "111111111111";
-  for ( int i = 0; i < ( sizeof(digits) / sizeof(int) ); i++ ) {
-    if ( digits[i] > 0 ) {
-      gamma[i] = '1';
-      epsilon[i] = '0';
+  // Count the number of 1's and 0's in the MSB position of the dataset
+  int firstBitCount[2] = { 0, 0 };
+  for ( int i = 0; i < rows; i++ ) {
+    if ( dataset[i][0] == 0 ) {
+      firstBitCount[0]++;
+    } else if ( dataset[i][0] == 1 ) {
+      firstBitCount[1]++;
+    }
+  }
+  int dominantBit = firstBitCount[0] > firstBitCount[1] ? 0 : 1;
+  int minorBit = firstBitCount[0] < firstBitCount[1] ? 0 : 1;
+
+  // Reduce the array based on the "dominant" bit to find oxygen generator reading
+  int oxGen = 0;
+  reduce_by_dominant(dataset[0], rows, NUM_BITS, firstBitCount[dominantBit], dominantBit, 0, &oxGen);
+
+  // Reduce the array based on the "minor" bit to find CO2 scrubber reading
+  int scrubber = 0;
+  reduce_by_minor(dataset[0], rows, NUM_BITS, firstBitCount[minorBit], minorBit, 0, &scrubber);
+
+  // Construct the binary array for gamma and epsilon values bitwise
+  int gamma[NUM_BITS] = { 0 };
+  int epsilon[NUM_BITS] = { 0 };
+  for ( int i = 0; i < NUM_BITS; i++ ) {
+    if ( bitCount[i][1] > bitCount[i][0] ) {
+      gamma[i] = 1;
+    } else if ( bitCount[i][0] > bitCount[i][1] ) {
+      epsilon[i] = 1;
     }
   }
 
   // Convert the binary strings to decimal integers
-  long gamma_bin = atol(gamma);
-  int gamma_dec = bin_to_dec(gamma_bin);
-  long epsilon_bin = atol(epsilon);
-  int epsilon_dec = bin_to_dec(epsilon_bin);
+  int gamma_dec = binarray_to_dec(gamma, NUM_BITS);
+  int epsilon_dec = binarray_to_dec(epsilon, NUM_BITS);
 
-  printf ( "gamma: %s\nepsilon: %s\n", gamma, epsilon);
-  printf ( "gamma: %d\nepsilon: %d\n", gamma_dec, epsilon_dec );
+  printf ( "gamma: %d, epsilon: %d\n", gamma_dec, epsilon_dec );
   printf ( "power consumption: %d\n", gamma_dec * epsilon_dec );
+  printf ( "oxygen generator: %d, CO2 scrubber: %d\n", oxGen, scrubber );
+  printf ( "life support rating: %d\n", oxGen * scrubber );
 
   return EXIT_SUCCESS;
 }
